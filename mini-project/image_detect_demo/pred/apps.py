0932881e74e1382b150f92c2ba583b3c27194858
edowon0623@gmail.com <Dwlee$09>
2022-03-11 09:13:23 +0900

    added rnn materials

--- mini-project/image_detect_demo/pred/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PredConfig(AppConfig):
-    name = 'pred'
--- mini-project/image_detect_demo/settings_2.py
+++
@@ -1,8 +0,0 @@
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
-
-dotenv_path = join(dirname(__file__), 'settings_2.env')
-load_dotenv(dotenv_path)
-
-CASCADE_FILE_PATH = os.environ.get("CASCADE_FILE_PATH")
